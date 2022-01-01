002b92244af580aed73e18b4c04b07ebd4726bd8
Viviane Puga <vnpuga@gmail.com>
2022-01-01 10:17:27 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// requisito 5
+// requisito 5 e 6
 // recuperei o botão, input (caixa de texto) e ol (lista ordenada).
 const button = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa')
-const taskList = document.querySelector('#lista-tarefas');
+const OrderedList = document.querySelector('#lista-tarefas');
 
-function createLi(){
+function createLi() {
   const liTask = document.createElement('li'); // criar li
   liTask.innerText = input.value; // A propriedade value define ou retorna o valor de um atributo.
-  taskList.appendChild(liTask); // add li (filho) ao ol (pai)
-  input.value = '';
+  OrderedList.appendChild(liTask); // add li (filho) ao ol (pai)
+  input.value = ''; // input volta a ficar vazio, após criar li acima.
 }
 button.addEventListener('click',createLi);
+
+// requisito 7
+function backgroundColorLi() {
+  const liTask = document.getElementsByTagName('li');
+  for(let i = 0; i < liTask.length; i += 1) {
+    if(liTask[i].style.backgroundColor !== 'rgb(128, 128, 128)') {
+      liTask[i].style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+  }
+}
+OrderedList.addEventListener('click',backgroundColorLi);
+// item 7, ele esta atribuindo a cor a todos as 'li's dentro da 'ol', não consegui colocar a cor apenas em um item, mas passou no teste.
