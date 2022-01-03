5bb12be2d9bc58bfc19415fb5a9c440a77cf1ae7
Viviane Puga <vnpuga@gmail.com>
2022-01-03 10:05:21 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // requisito 5 e 6
 // recuperei o botão, input (caixa de texto) e ol (lista ordenada).
 const button = document.querySelector('#criar-tarefa');
-const input = document.querySelector('#texto-tarefa')
+const input = document.querySelector('#texto-tarefa');
 const orderedList = document.querySelector('#lista-tarefas');
 
 function createLi() {
@@ -10,7 +10,7 @@ function createLi() {
   orderedList.appendChild(liTask); // add li (filho) ao ol (pai)
   input.value = ''; // input volta a ficar vazio, após criar li acima.
 }
-button.addEventListener('click',createLi);
+button.addEventListener('click', createLi);
 
 // requisito 7
 // function backgroundColorLi() {
@@ -25,13 +25,25 @@ button.addEventListener('click',createLi);
 // item 7, ele esta atribuindo a cor a todos as 'li's dentro da 'ol', não consegui colocar a cor apenas em um item, mas passou no teste.
 
 // requisito 7 e 8
-//consegui fazer esse exercício com dica do Mateus Vital, p/ colocar cor branca em todas as li's, e mudar apenas na que eu clicar.
+// consegui fazer esse exercício com dica do Mateus Vital, p/ colocar cor branca em todas as li's, e mudar apenas na que eu clicar.
 function colorItemLi(event) {
   const liTask = document.getElementsByTagName('li'); // recuperei li's
   for (let i = 0; i < liTask.length; i += 1) {
     liTask[i].style.backgroundColor = 'white'; // todas as li's terão fundo branco
   }
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)'; // evento que muda a cor apenas do item clicado
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)'; // evento que muda a cor apenas do item clicado
 }
 
 orderedList.addEventListener('click', colorItemLi);
+
+// requisito 9
+// o método contais() retorna true ou false para o parâmetro buscado dentro de parênteses
+function itemRiscado(event){
+  const liTask = document.getElementsByTagName('li'); // recuperei li's
+  if(event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed'); // remover a classe após clique duplo
+  } else {
+    event.target.classList.add('completed'); // adicionar a classe após clique duplo
+  }  
+}
+orderedList.addEventListener('dblclick', itemRiscado);
